7525bca0bf5c7fdaa69c039e9ece9cec0086196e
Fahmedo <faruqahmedolaitan@gmail.com>
2023-09-15 16:54:59 +0100

    new commit

--- src/components/TopMovies.jsx
+++ src/components/TopMovies.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import './movies.css';
+import './Movies.css';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Loader from '../utilities/Loader';
